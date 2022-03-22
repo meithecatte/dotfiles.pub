71a7bae5f66811c34370fa066398961eb93a3b61
Jakub Kądziołka <kuba@kadziolka.net>
2022-03-22 15:43:21 +0100

    nixos: Bluetooth setup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -168,9 +168,48 @@
   # nsjail needs this
   systemd.enableUnifiedCgroupHierarchy = false;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # Use pipewire over pulseaudio for high quality microphone audio over Bluetooth.
+  sound.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            # mSBC is not expected to work on all headset + adapter combinations.
+            "bluez5.msbc-support" = true;
+            # SBC-XQ is not expected to work on all headset + adapter combinations.
+            "bluez5.sbc-xq-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
+  };
+
+
+  hardware.bluetooth.enable = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.enableAllFirmware = true;
+  nixpkgs.config.allowUnfree = true;
+  services.blueman.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver = {
